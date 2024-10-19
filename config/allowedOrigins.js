98de8d50f6c5f1cfc9ece5cb5e28143d32473bcd
WilliamsiQ <68313949+WilliamsiQ@users.noreply.github.com>
2024-10-19 23:46:20 +0100

    Update allowedOrigins.js
    update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 //cross origin resource sharing
 const allowedOrigin = [
-  "https://www.yoursite.com",
+  
   "http://127.0.0.1:5500",
   "http://localhost:3500",
 ]; // domains that can access backend server of course the last two in the array should be omitted
